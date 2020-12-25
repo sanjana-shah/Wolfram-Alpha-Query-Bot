2d04720089ae165235cdeb3a47fd7cad0f186eee
Sanjana <53399985+sanjana-shah@users.noreply.github.com>
2020-12-25 11:14:57 +0530

    Update bot.py
    Added Google Text to Speech API to speak the answers

--- bot.py
+++ bot.py
@@ -1,28 +1,29 @@
-#Source code from GeeksforGeeks and ideas from https://pypi.org/project/wolframalpha/
-
-
 # Python program to  
 # demonstrate creation of an 
 # assistant using wolf ram API 
   
 import wolframalpha 
+from gtts import gTTS
+import os
   
 # Taking input from user 
 question = input('Question: ') 
   
 # App id obtained by the above steps 
-app_id = 'App Id'
+app_id = 'HAQRKG-PLRP8P4RKK'
   
 # Instance of wolf ram alpha  
 # client class 
 client = wolframalpha.Client(app_id) 
-  
+language='en'
 # Stores the response from  
 # wolf ram alpha 
 res = client.query(question) 
   
 # Includes only text from the response 
 answer=next(res.results).text
-
-print(answer)
+myObj=gTTS(text=question + 'is' + answer,lang=language,slow=False)
+myObj.save("welcome.mp3")
+os.system("welcome.mp3")
   
+#print(answer) 
